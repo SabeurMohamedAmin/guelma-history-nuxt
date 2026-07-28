98330ee744a3a0775f4f444a91515c5e29af915d
sabeurmohammedamin <sabeurmohammedamin@gmail.com>
2026-07-29 00:22:06 +0200

    feat: implement guest header component with responsive navigation and drawer

--- app/components/layout/LayoutAppHeader.vue
+++ app/components/layout/LayoutAppHeader.vue
@@ -435,9 +435,9 @@ function toggleTheme() {
 }
 
 .app-header {
-  -webkit-backdrop-filter: blur(10px) brightness(.75);
-  backdrop-filter: blur(10px) brightness(.75);
-  background-color: rgb(var(--v-theme-surface) / 0.90);
+  -webkit-backdrop-filter: blur(10px) brightness(.5);
+  backdrop-filter: blur(10px) brightness(.5);
+  background-color: rgb(var(--v-theme-surface) / 0.20);
   border-bottom: 1px solid rgb(var(--v-border-color) / 0.10) !important;
   transition:
     top 0.3s cubic-bezier(0.16, 1, 0.3, 1),
